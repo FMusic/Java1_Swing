4e5d4e7b0a6d3c54b708cd11ac9124a91688344b
Frane Mušić <frane.music@gmail.com>
2020-02-01 21:51:03 +0100

    maybe finished design DoctorScreen

--- src/com/fm/ui/DoctorScreen.java
+++ src/com/fm/ui/DoctorScreen.java
@@ -1,15 +1,18 @@
 package com.fm.ui;
 
+import com.fm.controller.DoctorsController;
 import com.fm.controller.PatientsController;
-import com.fm.model.ComprehensivePatientEntity;
-import com.fm.model.MiniPatientEntity;
-import com.fm.model.PatientsFilesEntity;
+import com.fm.model.*;
 import com.fm.utils.SwingUtils;
 import com.sun.istack.Nullable;
+import org.jdatepicker.DatePicker;
+import org.jdatepicker.JDatePicker;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class DoctorScreen extends  JFrame{
@@ -21,9 +24,15 @@ public class DoctorScreen extends  JFrame{
     private JButton goButton;
     private JPanel panelCombo;
     private JPanel pnlBoxButtons;
+    private JPanel pnlDate;
+    private JList list1;
+    private JTextArea textArea1;
+    private JComboBox<StaffEntity> cbSpecs;
     JButton butOpenComprehensive;
     private JButton butNewFile;
     private PatientsFilesEntity[] listOfFiles;
+    JDatePicker picker;
+    StaffEntity[] specs;
 
     public DoctorScreen(){
         initData();
@@ -72,6 +81,18 @@ public class DoctorScreen extends  JFrame{
         for (int i = 0; i < listOfMinis.size(); i++) {
             listOfPatients[i] = listOfMinis.get(i);
         }
+        List<StaffEntity> listOfStaff = DoctorsController.getWholeStaff();
+
+        List<StaffEntity> listOfSpecs = new ArrayList<>();
+        listOfStaff.forEach(x-> {
+            if(x.getType().equals(new TypesEntity("Specialist"))){
+                listOfSpecs.add(x);
+            }
+        });
+        specs = new StaffEntity[listOfSpecs.size()];
+        for (int i = 0; i < listOfSpecs.size(); i++) {
+            specs[i] = listOfSpecs.get(i);
+        }
     }
 
     private void initWidgets() {
@@ -80,7 +101,10 @@ public class DoctorScreen extends  JFrame{
         pnlBoxButtons.setAlignmentX(SwingConstants.CENTER);
         pnlBoxButtons.setLayout(bl);
         butOpenComprehensive = new JButton("Open comprehensive form");
-        butNewFile = new JButton("New file for patient");
+        butNewFile = new JButton("Save file as new");
+        picker = new JDatePicker(Calendar.getInstance());
+        pnlDate.add(picker);
+        cbSpecs = new JComboBox<>(specs);
 //        doesnt work
 //        Dimension d = new Dimension(800,30);
 //        butOpenComprehensive.setSize(d);
@@ -93,6 +117,6 @@ public class DoctorScreen extends  JFrame{
     private void initForm() {
         SwingUtils.setPanel(panelCombo, cbPatients);
         setContentPane(pnlMain);
-        setSize(400, 300);
+        setSize(700, 300);
     }
 }
